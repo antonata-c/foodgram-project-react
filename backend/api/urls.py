3c08e9767c21f9889a43fcd8534c0ecca9e7db0f
Anton <anton_ata@mail.ru>
2023-09-20 16:11:41 +0300

    urls added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
-from .views import RecipeViewSet
+from .views import RecipeViewSet, IngredientViewSet, TagViewSet
 
-router = DefaultRouter()
-
+router = routers.DefaultRouter()
 router.register(r'recipes', RecipeViewSet, basename='recipes')
-
+router.register(r'ingredients',
+                IngredientViewSet,
+                basename='ingredients')
+router.register(r'tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
 
-from api.serializers import RecipeSerializer
-from recipes.models import Recipe
+from .serializers import RecipeSerializer, IngredientSerializer, TagSerializer
+from recipes.models import Recipe, Ingredient, Tag
 
 
-class RecipeViewSet(viewsets.ViewSet):
+class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
