184369fe429ba9c37d3054ccddb007aab75f77fc
Anton <anton_ata@mail.ru>
2023-10-11 18:53:58 +0300

    Add admin panel

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 
-from .models import Ingredient, Recipe, Tag
+from .models import (Favorite, Ingredient, RecipeIngredient,
+                     Recipe, ShoppingCart, Tag, )
 
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'name',)
+    list_filter = ('name', 'author', 'tags')
+
+
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    """
+    Админ-зона ингридиентов.
+    """
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Tag)
+admin.site.register(RecipeIngredient)
+admin.site.register(ShoppingCart)
+admin.site.register(Favorite)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Follow
+
+
+class UserAdmin(admin.ModelAdmin):
+    """
+    Админ-зона пользователя.
+    """
+    list_display = ('id', 'username', 'email',
+                    'first_name', 'last_name', 'role')
+    list_filter = ('username', 'email',)
+
 
 admin.site.register(User)
+admin.site.register(Follow)
